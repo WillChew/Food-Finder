5e6f2ab3e871ebb756e74baf2e5743f05964ad84
WillChew <williamchew95@gmail.com>
2019-02-02 04:31:31 -0500

    allows user to take/add a picture to restaurant they are saving

--- Food Finder/Controllers/NewEntryTableViewController.swift
+++ Food Finder/Controllers/NewEntryTableViewController.swift
@@ -17,19 +17,79 @@ class NewEntryTableViewController: UITableViewController {
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var tagsTextField: UITextField!
     
+    var imagePicker = UIImagePickerController()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(pickImage))
+        restaurantImage.addGestureRecognizer(imageTap)
+        
+        
+        restaurantImage.layer.borderColor = UIColor.black.cgColor
+        restaurantImage.layer.borderWidth = 2
+        
+        
+        
+        
+        
+        
+    }
+    
+    @objc func pickImage() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        alert.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { _ in
+            self.openGallery()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera(){
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = true
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Warning", message: "No camera detected", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func openGallery(){
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+}
+
+extension NewEntryTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            restaurantImage.image = editedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.isNavigationBarHidden = false
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
